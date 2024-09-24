ee10328ebd5cb0225582d2ef3628c34f0a00bc95
xchbx <ceina.ellison@gmail.com>
2024-09-24 15:27:29 -0400

    created helper functions

--- checklist.py
+++ checklist.py
@@ -1,9 +1,11 @@
+# Create Checklist
 checklist = list()
 # checklist.append('Blue')
 # print(checklist)
 # checklist.append('Orange')
 # print(checklist)
 
+# Define Functions
 def create(item):
     checklist.append(item)
 
@@ -16,6 +18,45 @@ def update(index, item):
 
 def destroy(index):
     checklist.pop(index)
+
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(str(index) + list_item)
+        index += 1
+# work on this section
+def mark_completed(index): 
+    for index in checklist:
+        completed = ()
+        if completed == "yes":
+            print(index.append("√"))
+
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+
+    # Read item
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+
+        # Remember that item_index must actually exist or our program will crash.
+        read(item_index)
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    # Catch all
+    else:
+        print("Unknown Option")
+
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
 
 def test():
     create("purple sox")
@@ -30,4 +71,18 @@ def test():
     print(read(0))
     # print(read(1))
 
+    # Call your new function with the appropriate value
+    select("C")
+    # View the results
+    list_all_items()
+    # Call function with new value
+    select("R")
+    # View results
+    list_all_items()
+    # Continue until all code is run
+
+    user_value = user_input("Please Enter a value:")
+    print(user_value)
+
+# Run Tests
 test()
